e0701ea98361920675dad0f1c70a51d773295839
MrEminent42 <github@leohorwitz.com>
2023-10-05 11:29:59 +0200

    basic pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
+import { Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Page1 from './pages/Page1';
+import Page2 from './pages/Page2';
 
 function App() {
   return (
-    <div>
-      <WelcomeBox2>
+    <FontWrapper>
+      <Navbar />
+      <MainContainer>
         Hello there! Welcome to Casamico.
-      </WelcomeBox2>
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/1" element={<Page1 />} />
+          <Route path="/2" element={<Page2 />} />
+        </Routes>
+      </MainContainer>
+    </FontWrapper>
   );
 }
 
 export default App;
 
-const WelcomeBox2 = styled.div`
-  background-color: #00bc74;
+const FontWrapper = styled.div`
+  font-family: Arial, Helvetica, sans-serif;
+`
+const MainContainer = styled.div`
   border-radius: 30px;
   padding: 100px 10px;
-  color: white;
+  color: black;
   text-align: center;
-  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
 `
---
+++ src/pages/Page2.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page2 = () => {
+    return (
+        <div>THIS IS PAGE 2!</div>
+    )
+}
+
+export default Page2
