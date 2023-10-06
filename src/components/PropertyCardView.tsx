a7fba75d10ed10f0f2b5dbc71dd4ec600204dbd7
bri-personal <107887789+bri-personal@users.noreply.github.com>
2023-10-06 11:10:01 +0200

    changed PropertyCardViewContainer to grid display to simplify

--- src/components/PropertyCardView.tsx
+++ src/components/PropertyCardView.tsx
@@ -5,76 +5,56 @@ import defaultHouseImage from '../assets/default_house.png';
 const PropertyCardView = () => {
     const navigate = useNavigate();
 
-    let cardW = window.innerWidth * 29 / 100;
-    let cardMargin = window.innerWidth / 100;
-
     return (
-        <PropertyCardViewContainer>
-            <PropertyCardRowCenterContainer>
-                <PropertyCardRowContainer>
-                    <PropertyCardContainer onClick={() => navigate("/2")} style={{ width: cardW, marginLeft: cardMargin, marginRight: cardMargin }}>
-                        <img src={defaultHouseImage} alt="Default House" />
-                        <div> Property 1</div>
-                        <div> 1 Task </div>
-                    </PropertyCardContainer>
+        <div>
+            <PropertyCardViewContainer>
+                <PropertyCardContainer onClick={() => navigate("/2")}>
+                    <img src={defaultHouseImage} alt="Default House" />
+                    <div> Property 1</div>
+                    <div> 1 Task </div>
+                </PropertyCardContainer>
 
-                    <PropertyCardContainer onClick={() => navigate("/2")} style={{ width: cardW, marginLeft: cardMargin, marginRight: cardMargin }}>
-                        <img src={defaultHouseImage} alt="Default House" />
-                        <div> Property 2</div>
-                        <div> 2 Tasks </div>
-                    </PropertyCardContainer>
+                <PropertyCardContainer onClick={() => navigate("/2")}>
+                    <img src={defaultHouseImage} alt="Default House" />
+                    <div> Property 2</div>
+                    <div> 2 Tasks </div>
+                </PropertyCardContainer>
 
-                    <PropertyCardContainer onClick={() => navigate("/2")} style={{ width: cardW, marginLeft: cardMargin, marginRight: cardMargin }}>
-                        <img src={defaultHouseImage} alt="Default House" />
-                        <div> Property 3</div>
-                        <div> 0 Tasks </div>
-                    </PropertyCardContainer>
-                </PropertyCardRowContainer>
-            </PropertyCardRowCenterContainer>
+                <PropertyCardContainer onClick={() => navigate("/2")}>
+                    <img src={defaultHouseImage} alt="Default House" />
+                    <div> Property 3</div>
+                    <div> 0 Tasks </div>
+                </PropertyCardContainer>
 
-            <PropertyCardRowCenterContainer>
-                <PropertyCardRowContainer>
-                    <PropertyCardContainer onClick={() => navigate("/2")} style={{ width: cardW, marginLeft: cardMargin, marginRight: cardMargin }}>
-                        <img src={defaultHouseImage} alt="Default House" />
-                        <div> Property 4</div>
-                        <div> 1 Task </div>
-                    </PropertyCardContainer>
+                <PropertyCardContainer onClick={() => navigate("/2")}>
+                    <img src={defaultHouseImage} alt="Default House" />
+                    <div> Property 4</div>
+                    <div> 1 Task </div>
+                </PropertyCardContainer>
 
-                    <PropertyCardContainer onClick={() => navigate("/2")} style={{ width: cardW, marginLeft: cardMargin, marginRight: cardMargin }}>
-                        <img src={defaultHouseImage} alt="Default House" />
-                        <div> Property 5</div>
-                        <div> 2 Tasks </div>
-                    </PropertyCardContainer>
+                <PropertyCardContainer onClick={() => navigate("/2")}>
+                    <img src={defaultHouseImage} alt="Default House" />
+                    <div> Property 5</div>
+                    <div> 2 Tasks </div>
+                </PropertyCardContainer>
 
-                    <PropertyCardContainer onClick={() => navigate("/2")} style={{ width: cardW, marginLeft: cardMargin, marginRight: cardMargin }}>
-                        <img src={defaultHouseImage} alt="Default House" />
-                        <div> Property 6</div>
-                        <div> 0 Tasks </div>
-                    </PropertyCardContainer>
-                </PropertyCardRowContainer>
-            </PropertyCardRowCenterContainer>
-        </PropertyCardViewContainer>
+                <PropertyCardContainer onClick={() => navigate("/2")}>
+                    <img src={defaultHouseImage} alt="Default House" />
+                    <div> Property 6</div>
+                    <div> 0 Tasks </div>
+                </PropertyCardContainer>
+            </PropertyCardViewContainer>
+        </div>
     )
 }
 
 export default PropertyCardView
 
 const PropertyCardViewContainer = styled.div`
-    border-radius: 10px;
-    padding: 10px;
+    display: grid;
+    gap: 10px;
+    grid-template-columns: auto auto auto;
     color: black;
-`
-
-const PropertyCardRowContainer = styled.div`
-    display: flex;
-    padding: 10px;
-    border: 1px dotted black;
-`
-
-const PropertyCardRowCenterContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
 `
 
 const PropertyCardContainer = styled.div`
