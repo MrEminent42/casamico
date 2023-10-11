27330d239bdbb36be2eeff71f0119e881c99c464
bri-personal <107887789+bri-personal@users.noreply.github.com>
2023-10-11 17:05:24 +0200

    made add property page work with new nav bar

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -3,7 +3,11 @@ import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
 import PropertyCard from '../components/PropertyCard';
 
-const AddPropertyPage = () => {
+interface AddEditPropertyProps {
+    setSelectedProperty: (property: string) => void;
+}
+
+const AddPropertyPage = (props: AddEditPropertyProps) => {
     let navigate = useNavigate();
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -13,8 +17,10 @@ const AddPropertyPage = () => {
 
     return (
         <div>
-            <h3> New Property </h3>
             <AddPropertyForm onSubmit={handleSubmit}>
+                <GridItemCol12>
+                    <h3> New Property </h3>
+                </GridItemCol12>
                 <GridItemCol12>
                     <TitleAndText title="Street Address" name="address" />
                 </GridItemCol12>
@@ -25,6 +31,7 @@ const AddPropertyPage = () => {
                             address="Preview Address"
                             image={"https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"}
                             numTasks={0}
+                            setSelectedProperty={props.setSelectedProperty}
                         />
                     </PreviewBorderContainer>
                 </PreviewContainer>
@@ -86,7 +93,7 @@ const AddPropertyForm = styled.form`
     margin: 0px 50px;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+    grid-template-rows: 0.5fr 1fr 1fr 1fr 1fr 1fr;
     gap: 10px;
     border: 1px dotted black;
 `
@@ -116,8 +123,8 @@ const PreviewContainer = styled.div`
     align-items: center;
     margin: auto;
     grid-column-start: 3;
-    grid-row-start: 1;
-    grid-row-end: 5;
+    grid-row-start: 2;
+    grid-row-end: 6;
     pointer-events: none;
     border: 1px dotted black;
 `
@@ -158,6 +165,6 @@ const SubmitButtonsContainer = styled.div`
     flex-direction: column;
     align-items: center;
     grid-column-start: 3;
-    grid-row-start: 5;
+    grid-row-start: 6;
     border: 1px dotted black;
 `
