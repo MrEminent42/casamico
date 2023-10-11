79e526fa2e1d56046e0b7955e32cddf65fb674d1
Geoff Blaylock <gblaylock2024@gmail.com>
2023-10-11 18:28:43 +0200

    task page looking good

--- src/pages/Page2.tsx
+++ src/pages/Page2.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import backbuttonsvg from '../images/arrow-left-circle.svg';
 import house from "../images/house.jpg";
 import addbuttonsvg from "../images/plus-button.svg";
+import FilterButton from '../components/FilterButton';
+import Task from '../components/Task';
 
 const Page2 = () => {
     return (
@@ -18,28 +20,22 @@ const Page2 = () => {
                 </HouseLabel>
             </HouseContainer>
             <FilterandSortContainer>
-                <FilterButton>
-                    Filter
-                </FilterButton>
-                <SortButton>
-                    Sort
-                </SortButton>
+                <FilterButton 
+                    label="Filter"
+                    onClick={() => console.log("Filter button clicked")}
+                />
+                <FilterButton
+                    label="Sort"
+                    onClick={() => console.log("Sort button clicked")}
+                />
                 <AddButton src={addbuttonsvg}></AddButton>
             </FilterandSortContainer>
             <TaskListContainer>
-                <BackgroundOfTaskContainer>
-                    <TheTaskContainer>
-                        <TextTaskTitle>
-                        Clean the Damn Dishes yurr
-                        </TextTaskTitle>
-                        <DaysLeftLabel>
-                            2 Days
-                        </DaysLeftLabel>
-                    </TheTaskContainer>
-                    <AddyLabel>
-                        Property 1
-                    </AddyLabel>
-                </BackgroundOfTaskContainer>
+                <Task
+                    title="Task 1"
+                    due="Due 1"
+                    bg_color="#E1CAE8"
+                />
             </TaskListContainer>
         </TaskContainer>
     )
@@ -48,20 +44,17 @@ const Page2 = () => {
 export default Page2
 
 const TaskContainer = styled.div`
-    border: 2px solid blue;
     display: flex;
     flex-direction: column;
 `
 
 const BackButtonContainer = styled.div`
-    border: 2px solid red;
     display: flex;
     align-items: center;
-    padding: 0;
+    margin: 10px 10px 10px 0;
 `
 
 const BackButton = styled.img`
-    border: 2px solid green;
     width: 30px;
     height: 30px;
     cursor: pointer;
@@ -75,16 +68,17 @@ const BackLabel = styled.p`
 `
 //container to hold the house photo and the text
 const HouseContainer = styled.div`
-    border: 2px solid blue;
     width: 80vw;
     height: 250px;
     position: relative;
-    border-radius: 10px;
+    border-radius: 12px;
 
     background-image: url(${house});
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
+
+    margin: 0 10px 10px 0;
 `
 //the overlay for the house image
 const HouseImageOverlay = styled.div`
@@ -95,6 +89,7 @@ const HouseImageOverlay = styled.div`
     height: 100px;
     background: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7));
     content: "";
+    border-radius: 12px;
 `
 
 const HouseLabel = styled.p`
@@ -103,47 +98,22 @@ const HouseLabel = styled.p`
     color: white;
     margin: 0;
     position: absolute;
-    bottom: 10px;
-    left: 15px;
+    bottom: 15px;
+    left: 20px;
+    font-weight: 600;
 `
 //container to hold the filter and sort buttons and add buttons
 const FilterandSortContainer = styled.div`
-    border: 2px solid brown;
     display: flex;
     align-items: center;
     justify-content: left;
     padding: 0;
     gap: 10px;
     width: 80vw;
-`
-//button for the filter option
-const FilterButton = styled.button`
-    background-color: #D9D9D9;
-    border: none;
-    color: #5F5F5F;
-    width: 100px;
-    height: 40px;
-    padding: 10px 0;
-    font-size: 16px;
-    margin: 5px 0;
-    cursor: pointer;
-    border-radius: 40px;
-`
-//button for the sort option
-const SortButton = styled.button`
-    background-color: #D9D9D9;
-    border: none;
-    color: #5F5F5F;
-    width: 100px;
-    height: 40px;
-    padding: 10px 0;
-    font-size: 16px;
-    margin: 5px 0;
-    cursor: pointer;
-    border-radius: 40px;
+
+    margin: 0 10px 10px 0;
 `
 const AddButton = styled.img`
-    border: 2px solid green;
     width: 40px;
     height: 40px;
     cursor: pointer;
@@ -151,57 +121,11 @@ const AddButton = styled.img`
 `
 //container for the task list
 const TaskListContainer = styled.div`
-    border: 2px solid purple;
-    display: flex;
-    align-items: left;
     padding: 0;
-    flex-direction: column;
-`
-
-const BackgroundOfTaskContainer = styled.div`
-    border: 2px solid red;
-    background-color: #DFC5FE;
     display: flex;
     align-items: left;
-    padding:0
     flex-direction: column;
-    height: 55px;
-    border-radius: 40px;
-`
-const TheTaskContainer = styled.div`
-    border: 2px solid orange;
-    background-color: #C994E3;
-    opacity:1;
-    display: flex;
-    align-items: left;
-    padding:0
-    flex-direction: column;
-    height: 55px;
-    border-radius: 40px;
-    width: 800px;
-`
 
-const TextTaskTitle = styled.p`
-    font-size: 28px;
-    color: black;
-    margin: 10px;
-    padding-left: 10px;
-    -webkit-text-stroke-width: .5px;
-    -webkit-text-stroke-color: ;
-`
+    margin: 0 10px 10px 0;
 
-const DaysLeftLabel = styled.p`
-    font-size: 28px;
-    color: grey;
-    margin: 10px;
-    margin-left: auto;
-    padding-right: 10px;
-`
-
-const AddyLabel = styled.p`
-    font-size: 28px;
-    color: grey;
-    margin: 10px;
-    margin-left: auto;
-    padding-right: 10px;
 `
