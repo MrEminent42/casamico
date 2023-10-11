53b67858a32d49a60b6581b344a6de73b85bbdf8
Zach Stone <zachstone30@gmail.com>
2023-10-11 12:10:39 +0200

    added the add button and fixed formatting

--- src/pages/Page2.tsx
+++ src/pages/Page2.tsx
@@ -31,6 +31,9 @@ const Page2 = () => {
                 <SortButton>
                     Sort
                 </SortButton>
+                <AddButton>
+                    +
+                </AddButton>
             </FilterandSortContainer>
             <TaskListContainer>
                 Task1
@@ -92,16 +95,17 @@ const HouseLabel = styled.p`
     -webkit-text-stroke-width: .5px;
     -webkit-text-stroke-color: black;
 `
-//container to hold the filter and sort buttons
+//container to hold the filter and sort buttons and add buttons
 const FilterandSortContainer = styled.div`
     border: 2px solid brown;
     display: flex;
     align-items: left;
     padding: 0;
+    
 `
 //button for the filter option
 const FilterButton = styled.button`
-    background-color: #4CAF50;
+    background-color: #a9a9a9;
     border: none;
     color: white;
     padding: 15px 32px;
@@ -111,12 +115,13 @@ const FilterButton = styled.button`
     font-size: 16px;
     margin: 4px 2px;
     cursor: pointer;
+    border-radius: 8px;
     
 
 `
 //button for the sort option
 const SortButton = styled.button`
-    background-color: #9FA355;
+    background-color: #a9a9a9;
     border: none;
     color: white;
     padding: 15px 32px;
@@ -126,7 +131,24 @@ const SortButton = styled.button`
     font-size: 16px;
     margin: 4px 2px;
     cursor: pointer;
+    border-radius: 8px;
     
+
+`
+//add task button
+const AddButton = styled.button`
+    background-color: #a9a9a9;
+    border: none;
+    color: white;
+    padding: 15px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 8px;
+    margin-left: auto 
 
 `
 //container for the task list
