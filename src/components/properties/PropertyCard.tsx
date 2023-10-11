d0323711fc182331e34f7766bdd081efe7efbf8d
MrEminent42 <github@leohorwitz.com>
2023-10-11 17:42:26 +0200

    fix typos for moving stuff around

--- src/components/properties/PropertyCard.tsx
+++ src/components/properties/PropertyCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { PropertyGridViewWrapper } from './PropertyCardView';
+import { PropertyGridViewWrapper } from '../../pages/Home';
 
 // check out Navbar.tsx for an explanation of this
 interface PropertyCardProps {
