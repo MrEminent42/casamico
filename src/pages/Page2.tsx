bfc394596d496349f30a3b8f7a324c2d7d5e3c77
Zach Stone <zachstone30@gmail.com>
2023-10-11 11:11:45 +0200

    Created a rough background and overlayed text

--- src/pages/Page2.tsx
+++ src/pages/Page2.tsx
@@ -10,6 +10,27 @@ const Page2 = () => {
                 <BackButton src={backbuttonsvg}></BackButton>
                 <BackLabel>back</BackLabel>
             </BackButtonContainer>
+            <HouseContainer>
+                <HouseImage style={{backgroundImage:
+                `url(${house})`, 
+                fontSize:'50px',
+                backgroundPosition: 'left',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                filter: 'blur(2px) opacity(80%)'}}>
+                </HouseImage>
+                <HouseLabel>
+                    PROPERTY 1
+                </HouseLabel>
+            </HouseContainer>
+            <FilterandSortContainer>
+                Yo
+            </FilterandSortContainer>
+            <TaskListContainer>
+                Task1
+                Task2
+                Task3
+            </TaskListContainer>
         </TaskContainer>
     )
 }
@@ -40,6 +61,40 @@ const BackLabel = styled.p`
     margin: 0;
 `
 const HouseContainer = styled.div`
-    border: 2px solid black;
+    border: 2px solid blue;
     display: flex;
+    width: 600px;
+    height: 400px;
+`
+const HouseImage= styled.img`
+    border: 2px solid yellow;
+    width: 600px;
+    height: 400px;
+`
+
+const HouseLabel = styled.p`
+    font-size: 85px;
+    color: white;
+    margin: 0;
+    position: absolute;
+    bottom: 200px;
+    left: 10px;
+    filter: drop-shadow(5px 5px 15px black);
+    -webkit-text-stroke-width: .5px;
+    -webkit-text-stroke-color: black;
+`
+
+const FilterandSortContainer = styled.div`
+    border: 2px solid brown;
+    display: flex;
+    align-items: left;
+    padding: 0;
+`
+
+const TaskListContainer = styled.div`
+    border: 2px solid purple;
+    display: flex;
+    align-items: left;
+    padding: 0;
+    flex-direction: column;
 `
