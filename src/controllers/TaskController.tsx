5a9c59a4daf540a40c9353babb64a42b61e8dc23
Geoff Blaylock <gblaylock2024@gmail.com>
2023-10-25 10:02:34 +0200

    fixes

--- src/controllers/TaskController.tsx
+++ src/controllers/TaskController.tsx
@@ -6,6 +6,7 @@ export const getTasksOfProperty = async (propertyId: number) => {
         .from('Tasks')
         .select()
         .eq('property_id', propertyId)
+        .order('due_date', { ascending: true })
 
     if (res.error) {
         throw (res.error);
