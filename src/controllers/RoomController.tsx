095f966c699e9b16f29668e3cb9b9c5d9e142e78
bri-personal <107887789+bri-personal@users.noreply.github.com>
2023-10-27 09:52:07 +0200

    delete rooms that are removed from the list when editing property

--- src/controllers/RoomController.tsx
+++ src/controllers/RoomController.tsx
@@ -14,19 +14,40 @@ export const getRooms = async (property_id: Database['public']['Tables']['Proper
     return res.data;
 }
 
+export const deleteRoom = async (room_id: number) => {
+    const { error } = await supabase
+        .from('Rooms')
+        .delete()
+        .eq('room_id', room_id);
+
+    if (error) {
+        throw (error);
+    }
+}
+
 export const createRooms = async (input: string, property_id: number) => {
     //parse comma separated string
     const names = input.split(',');
 
+    //get existing room names to avoid making duplicates later
     let existingRooms = await getRooms(property_id);
     let existingNames = new Set<string>();
-    existingRooms.forEach(room => {
-        existingNames.add(room.name);
+
+    //check all existing rooms
+    //if name is also in values retrieved from UI, add it to existing names
+    //if name not in values retrieved from UI, delete it from database
+    existingRooms.forEach(async room => {
+        if (names.includes(room.name)) {
+            existingNames.add(room.name);
+        }
+        else {
+            await deleteRoom(room.room_id);
+        }
     })
 
     //store new Room entry in database for each name and add room_id of each to returned array
     let room_ids = await Promise.all(names.map(async name => {
-        if (name && !existingNames.has(name)) {
+        if (name && !existingNames.has(name)) { //ensure room name isn't empty string or already used
             const { data, error } = await supabase
                 .from('Rooms')
                 .insert({ name: name, property_id: property_id })
@@ -36,10 +57,12 @@ export const createRooms = async (input: string, property_id: number) => {
                 throw (error);
             }
 
+            existingNames.add(data[0].name); //include newly created room names to avoid future duplicates
+
             return data[0].room_id;
         }
     }
     ));
 
-    return room_ids; //return array of room ids of rooms created
+    return room_ids; //return array of room ids of rooms created (NEW ONES ONLY)
 }
