60081902e73e674ff2c4d2a71af72f4424875e84
bri-personal <107887789+bri-personal@users.noreply.github.com>
2023-11-20 18:41:44 +0100

    refactor task tests to use beforeAll and afterAll

--- test/AddTaskController.test.ts
+++ test/AddTaskController.test.ts
@@ -1,14 +1,25 @@
 import { Database } from "../src/supabase/supabase";
 import { addTask } from "../src/controllers/AddTaskController"
 import { updateTask } from "../src/controllers/UpdateTaskController"
-import { test_manualCreateProperty, test_manualCreateRoom, test_manualCreateTask, test_manualDeleteProperty } from "./TestUtil";
+import { test_manualCreateProperty, test_manualCreateRoom, test_manualCreateTask, test_manualDeleteProperty, test_manualDeleteTask } from "./TestUtil";
 
 describe('tasks', () => {
+    let property: Database['public']['Tables']['Properties']['Row'];
+    let room: Database['public']['Tables']['Rooms']['Row'];
+
+    beforeAll(async () => {
+        // create temp property and room for tasks to correspond to
+        property = await test_manualCreateProperty();
+        room = await test_manualCreateRoom(property.property_id);
+    })
+
+    afterAll(async () => {
+        // delete temp property cascades to delete room too
+        await test_manualDeleteProperty(property.property_id);
+    })
+
     describe('adding a task', () => {
         test('should create a task with a unique task id', async () => {
-            const property = await test_manualCreateProperty();
-            const room = await test_manualCreateRoom(property.property_id);
-
             // create object test task to add to database
             // task_id must be specified so we know that function returns correct ID
             const task: Database['public']['Tables']['Tasks']['Insert'] = {
@@ -18,17 +29,15 @@ describe('tasks', () => {
                 room_id: room.room_id
             }
 
-            //returned value from adding task is its task_id
+            // check that task title is correct
             let res = await addTask(task);
             expect(res).toHaveProperty("title", "AUTOMATED TESTING TASK");
 
-            // tests should be independent of each other, so we need to delete all created entries
-            await test_manualDeleteProperty(property.property_id); // cascade delete from property will delete rooms and tasks associated with it
+            // tests should be independent, so remove created task
+            await test_manualDeleteTask(res.task_id);
         })
 
         test('should throw error if task_id is duplicate', async () => {
-            const property = await test_manualCreateProperty();
-            const room = await test_manualCreateRoom(property.property_id);
             const testTask = await test_manualCreateTask(property.property_id, room.room_id);
 
             // create object test task to add to database
@@ -37,29 +46,28 @@ describe('tasks', () => {
                 task_id: testTask.task_id
             }
 
+            // check that task title is correct
             await expect(addTask(task)).rejects.toThrowError("duplicate key");
 
-            // tests should be independent of each other, so we need to delete all created entries
-            await test_manualDeleteProperty(property.property_id); // cascade delete from property will delete rooms and tasks associated with it
+            // tests should be independent, so remove created task
+            await test_manualDeleteTask(testTask.task_id);
         })
     })
 
     describe('updating a task', () => {
         test('should change the task entry to match the new data', async () => {
-            const property = await test_manualCreateProperty();
-            const room = await test_manualCreateRoom(property.property_id);
             const task = await test_manualCreateTask(property.property_id, room.room_id);
 
-            //returned value from adding task is its task_id
+            // check that task title is correct
             let res = await updateTask({ ...task, title: task.title + " 2" }, task.task_id);
             expect(res).toHaveProperty("title", "AUTOMATED TESTING TASK 2");
 
-            // tests should be independent of each other, so we need to delete all created entries
-            await test_manualDeleteProperty(property.property_id); // cascade delete from property will delete rooms and tasks associated with it
+            // tests should be independent, so remove created task
+            await test_manualDeleteTask(res.task_id);
         })
 
         test('should throw error if no task with given id exists', async () => {
-            //returned value from adding task is its task_id
+            // check that error is thrown
             await expect(updateTask({
                 title: "AUTOMATED TESTING TASK",
                 due_date: "2023-01-01",
