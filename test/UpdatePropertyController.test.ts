8aa23a0bdbd78ef698ae8af6d6931d9859bafb95
Geoff Blaylock <gblaylock2024@gmail.com>
2023-11-21 17:10:21 +0100

    update property tests

---
+++ test/UpdatePropertyController.test.ts
@@ -0,0 +1,33 @@
+import { Database } from "../src/supabase/supabase";
+import { test_manualCreateProperty, test_manualCreateRoom, test_manualCreateTask, test_manualDeleteProperty, test_manualDeleteTask } from "./TestUtil";
+import { updateProperty } from "../src/controllers/UpdatePropertyController";
+
+describe('updating a property', () => {
+    let property: Database['public']['Tables']['Properties']['Row'];
+    let room: Database['public']['Tables']['Rooms']['Row'];
+
+    beforeAll(async () => {
+        // create temp property and room for tasks to correspond to
+        property = await test_manualCreateProperty();
+        room = await test_manualCreateRoom(property.property_id);
+    })
+
+    afterAll(async () => {
+        // delete temp property cascades to delete room too
+        await test_manualDeleteProperty(property.property_id);
+    })
+
+    // test update property (remove room)
+    test('should update the property entry to match the new data', async () => {
+        let res = await updateProperty(property, '');
+
+        expect(res).toHaveProperty("room_ids", []);
+    })
+
+    // test throw error if no property was updated
+    test('should throw error if no property was updated', async () => {
+        property.property_id = -1;
+
+        await expect(updateProperty(property, '')).rejects.toThrowError("No property was updated");
+    })
+})
